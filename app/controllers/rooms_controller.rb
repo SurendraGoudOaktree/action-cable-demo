31a09778c837d5b31295c01dd89846fc46b12fc3
Surendra Goud <surendra.goud@oaktreecloud.com>
2024-05-01 19:14:23 +0530

    Authenticate user while creating room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,5 @@
 class RoomsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show, :index]
   before_action :set_room, only: %i[ show edit update destroy ]
 
   # GET /rooms or /rooms.json
